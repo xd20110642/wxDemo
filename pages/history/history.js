37d3791f1db6028e90404a0ae348cdb82a939300
xd20110642 <790057064@qq.com>
2018-04-26 17:32:04 +0800

    添加记录历史

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,23 +1,68 @@
 // pages/history/history.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    logs:[]
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载 页面初始化 options为页面跳转所带来的参数
    */
-  methods: {
+  onLoad: function (options) {
+    let log = wx.getStorageSync('calclogs');
+    this.setData({ "logs": log })
+  },
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let log = wx.getStorageSync('calclogs');
+    this.setData({"logs":log})
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
